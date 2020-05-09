0d554ef711c64344e126e6b938e43cfcd247c800
Srinat37 <srinath.reddy@techolution.com>
2020-05-09 19:47:41 +0530

    Reorder data in log files

---
+++ src/assessment/Reorder_Data_in_Log_Files.java
@@ -0,0 +1,33 @@
+package assessment;
+
+import java.util.Arrays;
+
+/**
+ * problem statement : https://leetcode.com/problems/reorder-data-in-log-files/
+ */
+public class Reorder_Data_in_Log_Files {
+
+    public String[] reorderLogFiles(String[] logs) {
+
+        Arrays.sort(logs, (log1, log2) -> {
+
+            String[] split1 = log1.split("\\s", 2);
+            String[] split2 = log2.split("\\s", 2);
+            boolean isDigit1 = isDigit(split1[1].charAt(0));
+            boolean isDigit2 = isDigit(split2[1].charAt(0));
+            if (!isDigit1 && !isDigit2) {
+                int cmp = split1[1].compareTo(split2[1]);
+                if (cmp != 0) {
+                    return cmp;
+                }
+                return split1[0].compareTo(split2[0]);
+            }
+            return isDigit1 ? (isDigit2 ? 0 : 1) : -1;
+        });
+        return logs;
+    }
+
+    private boolean isDigit(char ch) {
+        return (ch >= 48 && ch <= 57);
+    }
+}
